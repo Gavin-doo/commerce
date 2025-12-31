92d607a219b58ae7d75339d7859f2e5c49fb8125
Gavin-doo <gavindoo94@gmail.com>
2026-01-01 01:16:33 +0900

    products feature API Implemantation

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Products
 
 # Register your models here.
+
+class ProductsAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'stock', 'created_at', 'updated_at')
+    search_fields = ('name', 'description')
+    list_filter = ('created_at', 'updated_at')
+
+admin.site.register(Products)
+
+
+
--- products/views.py
+++ products/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from  rest_framework import generics
+from .models import Products
+from .serializers import ProductsSerializer
+from rest_framework.permissions import IsAdminUser, AllowAny
 
 # Create your views here.
+class ProductsListCreateView(generics.ListCreateAPIView):
+    queryset = Products.objects.all()
+    serializer_class = ProductsSerializer
+
+    def get_permissions(self):
+        if self.request.method == 'POST':
+            return [IsAdminUser()]
+        return [AllowAny()]
+    
+class ProductDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Products.objects.all()
+    serializer_class = ProductsSerializer
+
+    def get_permissions(self):
+        if self.request.method in ['PUT', 'PATCH', 'DELETE']:
+            return [IsAdminUser()]
+        return [AllowAny()]    
