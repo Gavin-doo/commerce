feb370bab28a63010d31c966ec7cd892dfb74e85
Gavin-doo <gavindoo94@gmail.com>
2026-01-04 00:33:40 +0900

    add Cart API with JWT Authentication

--- cart/models.py
+++ cart/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
-from products.models import Product
+from products.models import Products
 
 # Create your models here.
 
 class Cart(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    product = models.ForeignKey(Products, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField(default=1)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
