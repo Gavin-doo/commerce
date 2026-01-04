60d1bb6b1006dd2d87cfd93e0df35fcd82e155cc
Gavin-doo <gavindoo94@gmail.com>
2026-01-04 19:48:42 +0900

    register order model in admin.py

--- order/admin.py
+++ order/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
-
+from .models import Order, OrderItem
 # Register your models here.
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('id', 'customer', 'status', 'created_at', 'updated_at')
+    search_fields = ('customer__username', 'status')
+    list_filter = ('status', 'created_at')
+    ordering = ('-created_at',)
+
+@admin.register(OrderItem)
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = ('id', 'order', 'product', 'quantity', 'price')
+    search_fields = ('order__id', 'product__name')
+    list_filter = ('product',)
+    ordering = ('-order__created_at',)
